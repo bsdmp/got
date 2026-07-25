44589c67aa27da2338a72a1e67cb3c8bc819f2ce
Mikhail <mp39590@gmail.com>
2026-07-25 17:21:37 +0000

    manual: fixes

--- regress/cmdline/status.sh
+++ regress/cmdline/status.sh
@@ -876,7 +876,7 @@ test_status_multiple_gitignore_files() {
 }
 
 test_status_root_gitignore_applies_to_subdir() {
-	local testroot=`test_init test_status_root_gitignore_applies_to_subdir`
+	local testroot=`test_init status_root_gitignore_applies_to_subdir`
 
 	got checkout $testroot/repo $testroot/wt > /dev/null
 	ret=$?
